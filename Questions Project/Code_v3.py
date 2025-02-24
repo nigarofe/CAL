770a19b30f7d8b7dc379a7091b4a122a839c10f7
Nicholas Gabriel Rocha Ferreira <nigarofe@gmail.com>
2025-02-24 15:01:16 -0300

    def get_days_since_last_attempt(question_number)

--- Questions Project/Code_v3.py
+++ Questions Project/Code_v3.py
@@ -2,34 +2,50 @@ import csv
 import ast
 from prettytable import PrettyTable
 from prettytable import TableStyle
+from datetime import date, datetime
 
 table = PrettyTable()
 
 def get_vector_from_question(question_number, vector_name):
-    string_vector = csv_reader_list[question_number-1][vector_name]
+    string_vector = csv_reader_list[question_number][vector_name]
     python_vector = ast.literal_eval(string_vector)
     return(python_vector)
 
 def get_total_attempts(question_number):
     return len(get_vector_from_question(question_number, 'code_vector'))
 
+def get_days_since_last_attempt(question_number):
+    date_vector = get_vector_from_question(question_number, 'date_vector')
+    last_attempt_date = datetime.strptime(date_vector[-1], "%Y-%m-%d").date()
+    today = date.today()
+        # print(date_vector[1])
+    print(last_attempt_date)
+    # print(today)
+    # print((today - last_attempt_date).days)
+    
+    return (today - last_attempt_date).days
+
 with open('questions.csv', 'r', newline='') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter='\t')
     csv_reader_list = list(csv_reader)
-    
-    
-    print(get_vector_from_question(3, 'code_vector'))
-    print(get_total_attempts(3))
 
-    table.field_names = ['Question number', "Total attempts"]
+    # print(get_vector_from_question(3, 'code_vector'))
+    # print(get_total_attempts(3))
+    print(get_days_since_last_attempt(20));
+
+    ## Generate Table ##
+    table.field_names = ['Question number', "Days Since Last Attempt", "Total attempts"]
+
+
     for i in range(len(csv_reader_list)):
-        table.add_row([i+1, get_total_attempts(i)])
+        table.add_row([i, get_days_since_last_attempt(i), get_total_attempts(i)])
         # print(f"{i+1}   {get_total_attempts(i)}")
     
     table.set_style(TableStyle.DOUBLE_BORDER)
     print(table)
 
 
+    ## -------------- ##
 
 
 
